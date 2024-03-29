de382da0e78980c04e866e28057312a6194e3258
rohit15033 <81508504+rohit15033@users.noreply.github.com>
2024-03-30 01:33:23 +0700

    Backend Progress
    Added register implementation in database and also worked on login page  frontend

--- customer/register/register.php
+++ customer/register/register.php
@@ -1,12 +1,39 @@
 <?php 
     if(isset($_POST["signup"]))
     {
+        $db_host = "localhost";
+        $db_username = "root";
+        $db_password = "";
+        $db_name = "susmarketplace";
+
+        $conn = new mysqli($db_host, $db_username, $db_password, $db_name);
+        if ($conn -> connect_error)
+        {
+            die("Connection failed: ". $conn -> connect_error);
+        }
+
         $email = $_POST["email"];
         $password = $_POST["password"];
         $confirm_password = $_POST["confirm_password"];
+        if ($password == $confirm_password)
+        {
+            $insert_user_data_query = "INSERT INTO users (email, password) VALUES ('$email', '$password');";
+            $insert_user_data = mysqli_query($conn, $insert_user_data_query);
+        }
 
-        echo "$email";
-        echo "$password";
-        echo "$confirm_password";
+        $select_all_query = "SELECT * FROM users";
+        $select_all = mysqli_query($conn, $select_all_query);
+
+        echo "<table border cellpadding = 1>";
+        echo "<tr><th>Email</th><th>Password</th></tr>";
+        while($row = mysqli_fetch_array($select_all))
+        {
+            echo "<tr>";
+            echo "<td>" . $row['email'] . "</td>";
+            echo "<td>" . $row['password'] . "</td>";
+            echo "</tr>";
+        }
+
+        $conn->close();
     }
 ?>
