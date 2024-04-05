1b788eb09d550b2d8258e9dd0ed14c3c51fdfefb
rohit15033 <81508504+rohit15033@users.noreply.github.com>
2024-04-06 01:50:45 +0700

    Optimized Code
    Removed try catch as it was unneeded

--- customer/login/login.php
+++ customer/login/login.php
@@ -1,8 +1,7 @@
 <?php 
     session_start();
 
-    if(isset($_POST["login"]))
-    {
+    if(isset($_POST["login"])) {
         $email = $_POST["email"];
         $password = $_POST["password"];
 
@@ -11,34 +10,28 @@
         $db_password = "$password";
         $db_name = "susmarketplace";
 
-        try {
-            $conn = new mysqli($db_host, $db_username, $db_password, $db_name);
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-            
-            $sql = "SELECT * FROM users WHERE email = '$email'";
-            $result = mysqli_query($conn, $sql);
+        $conn = new mysqli($db_host, $db_username, $db_password, $db_name);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
 
-            if (mysqli_num_rows($result) == 1) {
-                $user = mysqli_fetch_assoc($result);
-                $hashedPassword = $user['password'];
+        $sql = "SELECT * FROM users WHERE email = '$email'";
+        $result = mysqli_query($conn, $sql);
 
-                if (password_verify($password, $hashedPassword)) {
-                    $_SESSION['logged_in'] = true;
-                    header("Location: ../homepage/homepage.html");
-                    exit();
-                } else {
-                    $_SESSION['error'] = "Invalid username or password. Please try again.";
-                    header("Location: login.html");
-                    exit();
-                }
+        if (mysqli_num_rows($result) == 1) {
+            $user = mysqli_fetch_assoc($result);
+            $hashedPassword = $user['password'];
+
+            if (password_verify($password, $hashedPassword)) {
+                $_SESSION['logged_in'] = true;
+                header("Location: ../homepage/homepage.html");
+                exit();
             } else {
                 $_SESSION['error'] = "Invalid username or password. Please try again.";
                 header("Location: login.html");
                 exit();
             }
-        } catch (Exception $e) {
+        } else {
             $_SESSION['error'] = "Invalid username or password. Please try again.";
             header("Location: login.html");
             exit();
